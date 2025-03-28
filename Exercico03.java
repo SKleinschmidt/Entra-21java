4927d7096fd13ad2d6f88d975ccf692b5771b78d
SKleinschmidt <sigfrid.kleisnchmidt@gmail.com>
2025-03-28 11:26:13 -0300

    Add files via upload

---
+++ Exercico03.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercico03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Insira um numero");
+		int numero = input.nextInt();
+		
+	for (int i = numero; i <= 30; i += 3) {
+		System.out.println(i + "");
+		
+	}
+	
+	}
+	}
+
+
