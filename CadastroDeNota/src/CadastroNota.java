a088be85b8f0de7eb59ae6e2a159457dbbed7253
SKleinschmidt <sigfrid.kleisnchmidt@gmail.com>
2025-04-24 11:35:27 -0300

    Add files via upload

---
+++ CadastroDeNota/src/CadastroNota.java
@@ -0,0 +1,21 @@
+
+public class CadastroNota {
+	String dataRecebimento;
+	String notaFiscal;
+	String setor;
+	String fornecedor;
+	String codigoProduto;
+	String descricaoProduto;
+	double quantidadeRecebida;
+	double numeroDeCaixas;
+	double quantidadePorCaixa;
+	double quantidadeItens;
+	double pesoMedio;
+	double valorDaNota;
+	double valorUnitario;
+	double pesoMedioCaixa;
+	String recebidoPor;
+	String turno;
+	
+
+}
---
+++ CadastroDeNota/src/RegistroDeInformacao.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class RegistroDeInformacao {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Cadastro de Nota Fiscal");
+		
+		System.out.println("\nData de recebimento (DD-MM-AA");
+		String dataRecebimento = input.next();
+		
+		System.out.println("\n Número da nota fiscal");
+		String notaFiscal = input.next();
+		
+		System.out.println("Setor");
+		String setor = input.next();
+		
+		
+		
+		
+		
+
+	}
+
+}
